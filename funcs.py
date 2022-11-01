38e09d3870b4f8663bcf4d20e61a7bc5c5dd7468
gkhlevtov <ghkhlevtov@mail.ru>
2022-11-02 00:18:21 +0200

    initial commit

---
+++ funcs.py
@@ -0,0 +1,40 @@
+import pytesseract  # 0.3.10
+from cv2 import cv2  # opencv-python 4.5.4.58
+from googletrans import Translator  # 4.0.0rc1
+
+translator = Translator()  # Обозначаем переводчик
+config = r'--oem 3 --psm 6 -l jpn'  # Задаём параметры перевода
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'  # Указываем путь к tesseract
+lang_dict = {"Русский": "ru",
+             "Английский": "en",
+             "Немецкий": "de",
+             "Французский": "fr",
+             "Итальянский": "it",
+             "Испанский": "es",
+             "Украинский": "uk",
+             "Чешский": "cs",
+             "Сербский": "sr",
+             "Польский": "pl",
+             "Корейский": "ko"}  # Словарь языковых обозначений
+
+
+def translate(image_path, pronun=False, lang='en'):
+    """Функция перевода текста"""
+
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_text = pytesseract.image_to_string(img, config=config). \
+        replace('\n', ''). \
+        replace('.', '. '). \
+        replace(' ', ''). \
+        replace('', '')
+
+    result = translator.translate(text=img_text, src='ja', dest='en')
+
+    original_text = result.origin
+    pronunciation_text = str(translator.translate(text=img_text, src='ja', dest='ja').pronunciation)
+
+    if lang != 'en':
+        result = translator.translate(result.text, src='en', dest=lang)
+
+    return [original_text, pronunciation_text, result.text]
---
+++ main.py
@@ -0,0 +1,55 @@
+import sys
+
+from PIL import Image
+from PyQt5 import QtCore, QtWidgets
+from PyQt5 import uic
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+
+import funcs
+
+if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+
+if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+
+
+class JapaneseTranslator(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('design.ui', self)  # Загружаем дизайн
+        self.file_name = ''
+        self.translateButton.clicked.connect(self.translate)
+        self.fileButton.clicked.connect(self.image_choose)
+
+    def image_choose(self):
+        self.file_name = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '')[0]
+        self.prepared_image = Image.open(self.file_name).convert("RGBA")
+        width, height = self.prepared_image.size
+        im = self.prepared_image
+        data = im.tobytes("raw", "RGBA")
+        pixmap = QPixmap().fromImage(QImage(data, width, height, QImage.Format_RGBA8888))
+        self.image.setPixmap(pixmap)
+
+    def translate(self):
+        chosen_language = funcs.lang_dict[self.languageBox.currentText()]
+        self.japaneseText.setText('')
+        self.pronunciationText.setText('')
+        self.translatedText.setText('')
+
+        if self.file_name:
+            res = funcs.translate(image_path=self.file_name,
+                                  pronun=True,
+                                  lang=chosen_language)
+            self.japaneseText.setText(res[0])
+            self.translatedText.setText(res[2])
+            if self.pronunciationCheckBox.isChecked():
+                self.pronunciationText.setText(res[1])
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = JapaneseTranslator()
+    ex.show()
+    sys.exit(app.exec_())
